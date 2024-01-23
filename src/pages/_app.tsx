4942f36edf6c644bbe77b5b3183b77cbf758d420
Vinicius Ribeiro <viniciusgoes159@gmail.com>
2024-01-23 15:55:49 -0300

    estrutura da home

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import { globalStyles } from "@/styles/global";
 import type { AppProps } from "next/app";
 import logoImg from "../assets/logo.svg"
 import { Container, Header } from "@/styles/pages/app";
+import Image from "next/image";
 
 globalStyles();
 
@@ -12,9 +13,9 @@ export default function App({ Component, pageProps }: AppProps) {
     <Container>
 
       <Header>
-        <img src={logoImg.src} />
+        <Image src={logoImg} alt="Logo" />
       </Header>
 
-      <Component {...pageProps} />;
+      <Component {...pageProps} />
     </Container>)
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,31 @@
-import { styled } from "../styles"
+import { HomeContainer, Product } from "@/styles/pages/home"
+import Image from "next/image"
 
-const Button = styled('button', {
-  backgroundColor: '$green500',
-})
+import shirt1 from "../assets/shirts/1.png"
+import shirt2 from "../assets/shirts/2.png"
+import shirt3 from "../assets/shirts/3.png"
 
 function Home() {
   return (
-    <Button>clique aqui</Button>
+    <HomeContainer>
+      <Product>
+        <Image src={shirt1} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product>
+        <Image src={shirt2} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+    </HomeContainer>
   )
 }
 
