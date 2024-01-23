21df8f6b8c45433cb0afeaaa9349e6445fda5393
Vinicius Ribeiro <viniciusgoes159@gmail.com>
2024-01-23 18:27:12 -0300

    buscando dados do Stripe

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,13 @@
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+
+  images: {
+    remotePatterns: [
+      { hostname: 'files.stripe.com', }
+    ]
+  },
+
   reactStrictMode: true,
 };
 
